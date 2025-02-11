90336542d1625fbc9251fe31c0f8dd3967b14eec
AlexPorthApp <alejandro@porth.app>
2025-02-10 23:30:39 -0600

    Crear use cases para registrar usuario y hacer login (#4)
    Co-authored-by: AlexPV2792 <alexperezvalle@gmail.com>

---
+++ backend/src/application/useCases/RegisterUser.ts
@@ -0,0 +1,19 @@
+import { randomUUID } from "crypto";
+
+import { UserRepository } from "../../domain/repositories/UserRepository";
+import { User } from "../../domain/entities/User";
+import { BcryptService } from "../../infrastructure/security/BcryptService";
+
+export class RegisterUser {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute(name: string, email: string, password: string) {
+    const existingUser = await this.userRepository.findByEmail(email);
+    if (existingUser) throw new Error("User already exists");
+
+    const hashedPassword = await BcryptService.hashPassword(password);
+    const user = new User(randomUUID(), name, email, hashedPassword);
+
+    return this.userRepository.create(user);
+  }
+}
