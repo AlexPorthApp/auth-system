9ef9714d9b9c78206b0ea071e269afb4f7cb785b
AlexPV2792 <alexperezvalle@gmail.com>
2025-02-10 23:30:16 -0600

    Crear use cases para registrar usuario y hacer login

---
+++ backend/src/application/useCases/LoginUser.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+import { UserRepository } from "../../domain/repositories/UserRepository";
+import { BcryptService } from "../../infrastructure/security/BcryptService";
+
+export class LoginUser {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute(email: string, password: string) {
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) throw new Error("Invalid credentials");
+
+    const isPasswordValid = await BcryptService.comparePasswords(password, user.password);
+    if (!isPasswordValid) throw new Error("Invalid credentials");
+
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET!, {
+      expiresIn: "1h",
+    });
+
+    return { user, token };
+  }
+}
