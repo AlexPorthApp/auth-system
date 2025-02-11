bb788ee9e604998ae5b97b020ec092e897694410
AlexPorthApp <alejandro@porth.app>
2025-02-10 23:49:05 -0600

    Create API (#5)
    Co-authored-by: AlexPV2792 <alexperezvalle@gmail.com>

--- backend/src/application/useCases/LoginUser.ts
+++ backend/src/application/useCases/LoginUser.ts
@@ -8,9 +8,11 @@ export class LoginUser {
 
   async execute(email: string, password: string) {
     const user = await this.userRepository.findByEmail(email);
+
     if (!user) throw new Error("Invalid credentials");
 
     const isPasswordValid = await BcryptService.comparePasswords(password, user.password);
+    
     if (!isPasswordValid) throw new Error("Invalid credentials");
 
     const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET!, {
